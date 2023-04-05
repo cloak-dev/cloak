ddb3bc6158ca603529083650ee480f98f65d52ca
jyothssena G S <jyothssena02@gmail.com>
2023-04-05 22:23:45 +0530

    [feat] add websocket server and database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const { Server } = require("socket.io");
+const Database = require("better-sqlite3");
 
 const app = require("express")().get("/", (_req, res) => {
     res.sendFile(__dirname + "/index.html");
@@ -8,7 +9,21 @@ const server = require("node:http")
     .listen(7000, () => console.log("Listening on 7000"));
 
 new Server(server).on("connection", socket => {
-    socket.on("message", msg => {
+    socket.on("sendMessage", msg => {
         socket.broadcast.emit("message", msg);
+        deceitfullyStore(msg); // <---
     });
 });
+
+const db = new Database("messages.db");
+db.pragma("journal_mode = WAL");
+db.exec(
+    `
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL,
+        message TEXT NOT NULL,
+        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+    )
+    `
+);
