0f37a2a40c8078a351ecdb922de780b05aeec92e
Prathiba <prathiba642@gmail.com>
2023-04-05 22:44:46 +0530

    [feat] convert diffie-hellman to ECDH

--- snippets/final.js
+++ snippets/final.js
@@ -10,4 +10,29 @@ class E2EE {
     generateIv() {
         return crypto.getRandomValues(new Uint8Array(16));
     }
+
+    /**
+     * @see https://github.com/mdn/dom-examples/blob/master/web-crypto/derive-bits/ecdh.js
+     */
+    async generateKey() {
+        this.key = await window.crypto.subtle.generateKey(
+            { name: "ECDH", namedCurve: "P-256" },
+            false,
+            ["deriveBits"]
+        );
+    }
+    
+    getPublicKey() {
+        return { publicKey: this.key.publicKey };
+    }
+
+    async setRemotePublicKey(key) {
+        this.clientKey = key;
+
+        this.sharedSecret = await window.crypto.subtle.deriveBits(
+            { name: "ECDH", namedCurve: "P-256", public: this.clientKey },
+            this.key.privateKey,
+            256
+        );
+    }
 }
