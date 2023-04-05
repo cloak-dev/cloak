b686f36c7356d69720d1e7b6fe9441e593960c75
Shreejith <shreejithsg@gmail.com>
2023-04-05 23:11:05 +0530

    [feat] add content scripts

---
+++ chromium/content.js
@@ -0,0 +1,30 @@
+const url = chrome.runtime.getURL("assets/logo.png");
+
+const img = document.createElement("img");
+img.id = "cloak-logo";
+img.src = url;
+img.style.transform = "scale(0.5)";
+img.style.visibility = "hidden";
+
+const button = document.createElement("button");
+button.id = "cloak-button";
+button.style.position = "fixed";
+button.style.top = "10px";
+button.style.right = "10px";
+button.style.background = "none";
+button.style.outline = "none";
+button.style.fontSize = "1.2rem";
+button.style.visibility = "hidden";
+button.style.cursor = "pointer";
+button.innerText = "Start Encrypting";
+
+document.body.appendChild(button);
+document.body.appendChild(img);
+
+const s = document.createElement("script");
+
+const hostname = new URL(window.location.href).hostname;
+
+s.src = chrome.runtime.getURL(scripts[hostname].path);
+(document.head || document.documentElement).appendChild(s);
+s.onload = () => s.parentNode.removeChild(s);
